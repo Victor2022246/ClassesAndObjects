391116c9808c88358ac4546d482dbf7158655bed
Victor2022246 <2022246@student.cct.ie>
2023-10-04 13:11:38 +0100

    make a person class

---
+++ src/classes/objects/Person.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes.objects;
+
+import shapes.Triangle;
+
+/**
+ *
+ * @author victor pc
+ */
+public class Person {
+    int age;
+    double height;
+    String name;
+    double weight;
+   Triangle favTriangle;
+
+    public Person(int age, double height, String name, double weight, Triangle favTriangle) {
+        this.age = age;
+        this.height = height;
+        this.name = name;
+        this.weight = weight;
+        this.favTriangle = favTriangle;
+    }
+   
+   
+   
+       
+    
+}
