fc88cffd6c83ca6a59c66da1c1812a8d0e6eb6b0
Victor2022246 <2022246@student.cct.ie>
2023-10-04 12:46:46 +0100

    Triangle side Arrays
    made a triangle classes to keep track of the objects

--- src/classes/objects/ClassesObjects.java
+++ src/classes/objects/ClassesObjects.java
@@ -14,7 +14,33 @@ public class ClassesObjects {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Triangle trill1 = new Triangle;
+        Triangle trill2 = new Triangle;
+        Triangle trill3 = new Triangle;
+        Triangle trill4 = new Triangle;
+        Triangle trill5 = new Triangle;
+        Triangle trill6 = new Triangle;
+        
+        int[] sideLengths = new int [18];
+        //lengths of trill 1's side will be first, then the legths of trill2's side etc
+        sideLengths[0] = 5;
+        sideLengths[1] = 8;
+        sideLengths[2] = 9;
+        
+        // tri 5 (4,7,2)
+        sideLengths[12]=4;
+        sideLengths[13]= 7;
+        sideLengths[14] =2;
+        
+        
+        
+        // we want to set the middle side of each triangle to be 3 
+       sideLengths[0] = 5;
+        sideLengths[0] = 5;
+         sideLengths[0] = 5;
+          sideLengths[0] = 5;
+        
+        
     }
     
 }
---
+++ src/classes/objects/Triangle.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes.objects;
+
+/**
+ *
+ * @author victor pc
+ */
+public class Triangle {
+    int side1;
+    int side2;
+    int side3;
+}
